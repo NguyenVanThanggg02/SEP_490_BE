5eac6b4633b295672a389b6b26807afd6364c8f7
Datnd <nguyenducdat0610@gmail.com>
2024-11-11 22:06:51 +0700

    report

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,22 +1,28 @@
 import { reportsDao } from "../dao/index.js";
 
-const getAllReports = async(req, res) =>{
+const getAllReports = async (req, res) => {
   try {
-    const allReports = await reportsDao.fetchAllReports()
-    res.status(200).json(allReports)
+    const allReports = await reportsDao.fetchAllReports();
+    res.status(200).json(allReports);
   } catch (error) {
-    throw new Error(error.message)
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 const createReports = async (req, res) => {
   try {
     const { reasonId, userId, spaceId } = req.body;
+
+    // Validate input data
+    if (!reasonId || !userId || !spaceId) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
     const report = await reportsDao.createReports(reasonId, userId, spaceId);
-    res.status(200).json(report);
+    res.status(201).json(report);
   } catch (error) {
-    res.status(500).json({ error: error.toString() });
+    res.status(500).json({ error: error.message });
   }
 };
 
-export default { createReports,getAllReports };
+export default { createReports, getAllReports };
