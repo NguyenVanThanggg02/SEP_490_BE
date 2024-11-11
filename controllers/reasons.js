dd47189b29a315e74d1d03901055d424a50a2a43
Datnd <nguyenducdat0610@gmail.com>
2024-11-11 22:06:06 +0700

    updatereason

--- controllers/reasons.js
+++ controllers/reasons.js
@@ -2,10 +2,10 @@ import { reasonsDao } from "../dao/index.js";
 
 const getAllReasons = async (req, res) => {
   try {
-    const allReasons = await reasonsDao.fetchAllReasons();
-    res.status(200).json(allReasons);
+    res.status(200).json(await reasonsDao.fetchAllReasons());
   } catch (error) {
-    res.status(500).json({ error: error.toString() });
+    res.status(500).json({ error: error.message });
   }
 };
+
 export default { getAllReasons };
